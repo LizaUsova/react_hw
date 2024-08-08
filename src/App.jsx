6f7182e19e567aee7838c047c64d21a2a4bdf84b
Liza Usova <Liza.usova@gmail.com>
2024-08-08 16:54:06 +0300

    hw_82 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import Hello from './components/Hello'
+import React from "react";
+import ListGroup from './components/ListGroup'
 
 function App() {
   return (
       <div className="App">
-        <Hello />
+          <ListGroup>
+              <p>Cras justo odio</p>
+              <p>Dapibus ac facilisis in</p>
+              <p>Morbi leo risus</p>
+              <p>Porta ac consectetur ac</p>
+              <p>Vestibulum at eros</p>
+          </ListGroup >
       </div>
   );
 }
