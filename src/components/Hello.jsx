be33d0a8a0dce470d325a35e0ca61dc9b3ff4f68
Liza Usova <Liza.usova@gmail.com>
2024-08-17 23:33:48 +0300

    hw_87 step1

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -3,7 +3,12 @@ import classNames from "classnames";
 
 class Hello extends React.Component {
     render() {
-        return <h3>Hello!</h3>
+        return <div>
+            <div className="btn-group font-monospace" role="group">
+                <button type="button" className="btn btn-outline-success">+</button>
+                <button type="button" className="btn btn-outline-danger">-</button>
+            </div>
+        </div>
     }
 }
 export default Hello;
