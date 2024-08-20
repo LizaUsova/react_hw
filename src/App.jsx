57156053df7ad9b7b53376628d47b6e664de0b47
Liza Usova <Liza.usova@gmail.com>
2024-08-20 13:14:18 +0300

    hw_92 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import Hello from './components/Hello'
+import {Col, Container, Row} from 'react-bootstrap'
+import React, {Component} from "react";
+import MarkdownEditor  from './components/MarkdownEditor'
 
-function App() {
-  return (
-      <div className="App">
-        <Hello />
-      </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <main className="users">
+                <Container>
+                    <Row>
+                        <Col>
+                            <MarkdownEditor onContentChange={console.log} />
+                        </Col>
+                    </Row>
+                </Container>
+            </main>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/MarkdownEditor.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Editor } from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import PropTypes from 'prop-types';
+
+class MarkdownEditor extends Component {
+    constructor(props) {
+        super(props);
+        this.editorRef = React.createRef(); // Создаем реф для ссылки на DOM-элемент
+    }
+
+    componentDidMount() {
+        const element = this.editorRef.current;
+        const editor = new Editor({
+            el: element,
+            hideModeSwitch: true
+        });
+
+        editor.addHook('change', () => {
+            const content = editor.getMarkdown();
+            if (this.props.onContentChange) {
+                this.props.onContentChange(content);
+            }
+        });
+
+        this.editorInstance = editor;
+    }
+
+    componentWillUnmount() {
+        if (this.editorInstance) {
+            this.editorInstance.destroy();
+        }
+    }
+
+    render() {
+        return <div ref={this.editorRef}></div>;
+    }
+}
+
+MarkdownEditor.propTypes = {
+    onContentChange: PropTypes.func.isRequired
+};
+
+export default MarkdownEditor;
